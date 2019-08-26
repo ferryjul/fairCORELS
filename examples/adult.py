7119da2679db7bca655c6ea05facfd3dd87062fb
Julien Ferry <jferry@pinkfloyd.laas.fr>
2019-08-26 19:20:20 +0200

    Adds generic k fold cross validation example

--- examples/adult.py
+++ examples/adult.py
@@ -1,4 +1,4 @@
-from fairules import *
+from faircorels import *
 from metrics import ConfusionMatrix, Metric
 import pandas as pd
 
@@ -6,7 +6,7 @@ X, y, features, prediction = load_from_csv("data/adult_train_binary.csv")
 X_test, y_test, features_test, prediction_test = load_from_csv("data/adult_test_binary.csv")
 
 
-c = CorelsClassifier(n_iter=1000000, c=0.0045, max_card=1, policy="bfs", fairness=1, maj_pos=19, min_pos=18, epsilon=0.99, mode=3)
+c = CorelsClassifier(n_iter=1500000, c=0.0001, max_card=1, policy="bfs", bfs_mode=2, useUnfairnessLB=True, fairness=1, maj_pos=19, min_pos=18, epsilon=1.00, mode=3)
 
 c.fit(X, y, features=features, prediction_name="(income:>50K)")
 
@@ -32,5 +32,3 @@ print("=========> Statiscal parity {}".format(fm.statistical_parity()))
 print("=========> Predictive parity {}".format(fm.predictive_parity()))
 print("=========> Predictive equality {}".format(fm.predictive_equality()))
 print("=========> Equal opportunity {}".format(fm.equal_opportunity()))
-
-
---
+++ examples/kFoldCrossVal.py
@@ -0,0 +1,106 @@
+from faircorels import *
+from metrics import ConfusionMatrix, Metric
+import pandas as pd
+import numpy
+from joblib import Parallel, delayed
+
+def average(aList):
+    nb = 0
+    sumTot = 0
+    for El in aList:
+        nb = nb + 1
+        sumTot = sumTot + El
+    return sumTot/nb
+
+def performKFold(foldID):
+    startTest = int(foldID*setSize)
+    endTest =  int((foldID+1)*setSize)
+    startTrain1 = int(0)
+    endTrain1 = int(foldID*setSize - 1)
+    startTrain2 = int((foldID+1)*setSize + 1)
+    endTrain2 = int(len(X_tot))
+    print("fold test ", foldID, " : instances [", startTest, ", ", endTest, "]")
+    print("fold train ", foldID, " : instances [", startTrain1, ", ", endTrain1, "] and [", startTrain2, ", ", endTrain2, "]")
+    X_trainFold1 = X_tot[startTrain1:numpy.max([endTrain1,0]),:]
+    y_trainFold1 = y_tot[startTrain1:numpy.max([endTrain1,0])]
+    X_trainFold2 = X_tot[startTrain2:numpy.min([endTrain2,len(X_tot)-1]),:]
+    y_trainFold2 = y_tot[startTrain2:numpy.min([endTrain2,len(X_tot)-1])]
+    X_fold_train = numpy.concatenate((X_trainFold1, X_trainFold2), axis=0)
+    y_fold_train = numpy.concatenate((y_trainFold1, y_trainFold2), axis=0)
+    X_fold_test = X_tot[startTest:endTest,:]
+    y_fold_test = y_tot[startTest:endTest]
+    print("%d instances in test set, %d instances in train set" %(len(X_fold_test),len(X_fold_train)))
+    c = CorelsClassifier(n_iter=100000, c=0.001, max_card=1, policy="bfs", bfs_mode=2, useUnfairnessLB=True, fairness=1, maj_pos=UnprotectedIndex, min_pos=ProtectedIndex, epsilon=0.99, mode=3)
+    c.fit(X_fold_train, y_fold_train, features=features_tot, prediction_name="(income:>50K)")
+    # Accuracy sur le train set
+    accuracy_train = (c.score(X_fold_train, y_fold_train))
+    # Accuracy sur le test set
+    accuracy_test = (c.score(X_fold_test, y_fold_test))
+    # Fairness metrics sur le train set
+    y_pred_train = c.predict(X_fold_train)
+    cmTrain = ConfusionMatrix(X_fold_train[:,ProtectedIndex], X_fold_train[:,UnprotectedIndex], y_pred_train, y_fold_train)
+    cm_minorityTrain, cm_majorityTrain = cmTrain.get_matrix()
+    fmTrain = Metric(cm_minorityTrain, cm_majorityTrain)
+    # Fairness metrics sur le test set
+    y_pred_test = c.predict(X_fold_test)
+    cmTest = ConfusionMatrix(X_fold_test[:,ProtectedIndex], X_fold_test[:,UnprotectedIndex], y_pred_test, y_fold_test)
+    cm_minorityTest, cm_majorityTest = cmTest.get_matrix()
+    fmTest = Metric(cm_minorityTest, cm_majorityTest)
+    return [accuracy_train, accuracy_test, fmTrain.statistical_parity(), fmTrain.predictive_parity(), fmTrain.predictive_equality(), fmTrain.equal_opportunity(), fmTest.statistical_parity(), fmTest.predictive_parity(), fmTest.predictive_equality(), fmTest.equal_opportunity()]
+
+kFold = 10 # Enter here the number of folds for the k-fold cross-validation
+UnprotectedIndex = 19
+ProtectedIndex = 18
+X_tot, y_tot, features_tot, prediction_tot = load_from_csv("data/adult_full_binary.csv")
+print("--- DATASET INFO --- ")
+print("Nombre d'attributs : %d, Nombre d'instances : %d" % (len(features_tot),len(X_tot)))
+print("Prediction : %s" %prediction_tot)
+print("--------------------")
+
+print("Will perform %d-folds cross-validation" %kFold)
+setSize = (len(X_tot)) / kFold
+print("Fold size = %d instances" %setSize)
+
+
+returnList = Parallel(n_jobs=-1)(delayed(performKFold)(foldID=_foldID) for _foldID in range(kFold))
+    
+
+accuracy_list_test = []
+statistical_parity_list_test = []
+predictive_parity_list_test = []
+predictive_equality_list_test = []
+equal_opportunity_list_test = []
+
+accuracy_list_train = []
+statistical_parity_list_train = []
+predictive_parity_list_train = []
+predictive_equality_list_train = []
+equal_opportunity_list_train = []
+
+for aReturn in returnList:
+    accuracy_list_train.append(aReturn[0])
+    accuracy_list_test.append(aReturn[1])
+    statistical_parity_list_train.append(aReturn[2])
+    predictive_parity_list_train.append(aReturn[3])
+    predictive_equality_list_train.append(aReturn[4])
+    equal_opportunity_list_train.append(aReturn[5])
+    statistical_parity_list_test.append(aReturn[6])
+    predictive_parity_list_test.append(aReturn[7])
+    predictive_equality_list_test.append(aReturn[8])
+    equal_opportunity_list_test.append(aReturn[9])
+
+#print("-------------------------- Learned rulelist ------------------------------")
+#print(c.rl())
+
+print("----------- Training set metrics : ----------- ")
+print("Accuracy: %lf" %average(accuracy_list_train))
+print("=========> Statistical parity %lf" %average(statistical_parity_list_train))
+print("=========> Predictive parity %lf" %average(predictive_parity_list_train))
+print("=========> Predictive equality %lf" %average(predictive_equality_list_train))
+print("=========> Equal opportunity %lf" %average(equal_opportunity_list_train))
+print("----------- Test set metrics : --------------- ")
+print("Accuracy %lf" %average(accuracy_list_test))
+print("=========> Statistical parity %lf" %average(statistical_parity_list_test))
+print("=========> Predictive parity %lf" %average(predictive_parity_list_test))
+print("=========> Predictive equality %lf" %average(predictive_equality_list_test))
+print("=========> Equal opportunity %lf" %average(equal_opportunity_list_test))
