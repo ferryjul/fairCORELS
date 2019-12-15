c73da1340e7ebb0e29c3f1edc47ce84da90c3270
black-women-mirror <a.u.matchi@gmail.com>
2019-12-15 07:20:21 -0500

    scripts for experiments

--- experiments/experiments_parallel.py
+++ experiments/experiments_parallel.py
@@ -64,6 +64,8 @@ epsilon_low_regime = np.linspace(0.89, 0.949, num=10)
 epsilon_high_regime = np.linspace(0.95, 0.999, num=60)
 epsilon_range = [0.0] + [x for x in epsilon_low_regime] + [x for x in epsilon_high_regime] + [1.0]
 
+epsilon_range = [0.0, 0.80, 0.90]
+
 
 # loading dataset
 
