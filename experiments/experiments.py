04ae4bfbe246060a2f055ad3e09bfef975f86ce0
black-women-mirror <a.u.matchi@gmail.com>
2019-12-15 04:06:20 -0500

    script cluster

--- experiments/experiments.py
+++ experiments/experiments.py
@@ -59,7 +59,7 @@ if args.id==4:
     maj_pos = 2
 
 # parameters
-N_ITER = 5*10**1
+N_ITER = 5*10**6
 epsilon_low_regime = np.linspace(0.89, 0.949, num=10) 
 epsilon_high_regime = np.linspace(0.95, 0.999, num=60)
 epsilon_range = [0.0] + [x for x in epsilon_low_regime] + [x for x in epsilon_high_regime] + [1.0]
--- job_adult_1.sh
+++ job_adult_1.sh
@@ -6,6 +6,7 @@
 #SBATCH --mail-user=a.u.matchi@gmail.com
 #SBATCH --mail-type=ALL
 
+cd experiments
 
 python experiments.py --id=1 --metric=1
     
