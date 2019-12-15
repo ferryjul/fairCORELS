376ac527b31cde4cee3c033c6eb4c567b788ea64
black-women-mirror <a.u.matchi@gmail.com>
2019-12-15 03:54:48 -0500

    script cluster

--- experiments/job_adult_1.sh
+++ experiments/job_adult_1.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 #SBATCH --time=24:00:00
-#SBATCH --array=1-6
-#SBATCH --cpus-per-task=10
+#SBATCH --mincpus=10
 #SBATCH --mem-per-cpu=3072M    
 #SBATCH --account=def-gambsseb
 #SBATCH --mail-user=a.u.matchi@gmail.com
 #SBATCH --mail-type=ALL
 
 
-python experiments.py --id=2 --metric=$SLURM_ARRAY_TASK_ID 
+python experiments.py --id=1 --metric=1
     
