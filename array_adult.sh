24f3ee7bf32d4967b86678d8f39c489c3e1ed92e
black-women-mirror <a.u.matchi@gmail.com>
2019-12-15 08:10:31 -0500

    scripts for experiments

--- array_adult.sh
+++ array_adult.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --time=24:00:00
-#SBATCH --array=1-6
+#SBATCH --array=1-6%1
 #SBATCH --cpus-per-task=10
-#SBATCH --mem-per-cpu=5120M    
+#SBATCH --mem-per-cpu=3072M    
 #SBATCH --account=def-gambsseb
 #SBATCH --mail-user=a.u.matchi@gmail.com
 #SBATCH --mail-type=ALL
