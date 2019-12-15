02f3d69119bed343fe09e28d13868cfba9f26e95
black-women-mirror <a.u.matchi@gmail.com>
2019-12-15 00:24:45 -0500

    sript for experiments

--- experiments/simple_job.sh
+++ experiments/simple_job.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 #SBATCH --time=00:15:00
 #SBATCH --account=def-gambsseb
+#SBATCH --mail-user=a.u.matchi@gmail.com
+#SBATCH --mail-type=ALL
+
 echo 'Hello, world!' > demo.txt
 sleep 30
 
 
-#SBATCH --mail-user=a.u.matchi@gmail.com
-#SBATCH --mail-type=BEGIN
-#SBATCH --mail-type=END
-#SBATCH --mail-type=FAIL
-#SBATCH --mail-type=REQUEUE
-#SBATCH --mail-type=ALL
+
