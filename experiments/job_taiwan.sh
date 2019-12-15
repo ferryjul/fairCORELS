42e7c63df7bde78569ebfb40ba17f1c3cc510c3d
Ulrich Aivodji <umaivodj@ip16.m>
2019-12-15 02:40:54 -0500

    results

---
+++ experiments/job_taiwan.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --time=24:00:00
+#SBATCH --array=1-6
+#SBATCH --cpus-per-task=10
+#SBATCH --mem-per-cpu=3072M    
+#SBATCH --account=def-gambsseb
+#SBATCH --mail-user=a.u.matchi@gmail.com
+#SBATCH --mail-type=ALL
+
+
+python experiments.py --id=4 --metric=$SLURM_ARRAY_TASK_ID 
+    
