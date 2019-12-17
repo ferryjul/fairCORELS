b98f10838042b2a42100919ad2abaf9f7dd694f1
black-women-mirror <a.u.matchi@gmail.com>
2019-12-16 23:31:06 -0500

    script for experiments

--- array_adult.sh
+++ array_adult.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-#SBATCH --time=24:00:00
+#SBATCH --time=00:30:00
 #SBATCH --array=1-6
-#SBATCH --cpus-per-task=10
-#SBATCH --mem-per-cpu=3G   
+#SBATCH --cpus-per-task=11
+#SBATCH --mem=20G  
+
+
 #SBATCH --account=def-gambsseb
 #SBATCH --mail-user=a.u.matchi@gmail.com
 #SBATCH --mail-type=ALL
 
 cd experiments
 
-python experiments.py --id=1 --metric=$SLURM_ARRAY_TASK_ID 
+python experiments.py --id=1 --metric=$SLURM_ARRAY_TASK_ID --attr=1
     
