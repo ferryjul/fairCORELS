6044386801b13319cc61881018b551279795bf82
black-women-mirror <a.u.matchi@gmail.com>
2019-12-16 21:34:00 -0500

    script for experiments

--- train_adult.sh
+++ train_adult.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --time=12:00:00
-#SBATCH --mem-per-cpu=20G  
 #SBATCH --cpus-per-task=10
-#SBATCH --nodes=10
+#SBATCH --mem=30G  
+
 
 #SBATCH --account=def-gambsseb
 #SBATCH --mail-user=a.u.matchi@gmail.com
@@ -12,8 +12,4 @@ cd experiments
 
 python experiments.py --id=1 --metric=1
     
-
-
-
-
 
