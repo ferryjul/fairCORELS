0da5d644565addd56aab0d30ef05699b77f00c66
black-women-mirror <a.u.matchi@gmail.com>
2019-12-17 22:23:59 -0500

    script for experiments

--- experiments/experiments_parallel.py
+++ experiments/experiments_parallel.py
@@ -131,8 +131,7 @@ def trainFold(X_train, y_train, X_test, y_test, epsilon, fairness_metric):
 
     clf.fit(X_train, y_train, features=features, prediction_name=prediction_name)
 
-    del clf
-    
+        
     """#test
     df_test = pd.DataFrame(X_test, columns=features)
     df_test[decision] = y_test
