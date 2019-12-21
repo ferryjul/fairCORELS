34f0eb1f5572f1689871c9eedaf9a717b6d1b150
black-women-mirror <a.u.matchi@gmail.com>
2019-12-20 21:56:08 -0500

    script for experiments

---
+++ scripts/param.sh
@@ -0,0 +1,16 @@
+export var_time=00:02:00
+
+export var_array=1-6
+
+export var_cpus=10
+
+export var_mem=30G
+
+export var_nodes=1
+
+export var_account=def-gambsseb
+
+export var_mail=a.u.matchi@gmail.com
+
+
+
