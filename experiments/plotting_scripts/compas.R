913d20bd2d64975177bed349f7ab80df93850c3c
black-women-mirror <a.u.matchi@gmail.com>
2019-12-28 07:34:42 -0500

    script for experiments

--- experiments/plotting_scripts/compas.R
+++ experiments/plotting_scripts/compas.R
@@ -1,133 +1,112 @@
 library(ggplot2)
 library(scales)
 
+#scale_x_continuous(breaks = seq(0.1, 10000, 500))
 
-metric <- "statistical_parity"
-with_file <- sprintf("../pareto/compas_%s_with_dem.csv", metric)
-without_file <- sprintf("../pareto/compas_%s_without_dem.csv", metric)
-with_laftr_file <- sprintf("../pareto/compas_%s_with_dem_laftr.csv", metric)
-without_laftr_file <- sprintf("../pareto/compas_%s_without_dem_laftr.csv", metric)
+point_size <- 3.5
+line_size <- 0.5
 
+#Statistical parity
+metric      <- "statistical_parity"
+file_us     <- sprintf("../pareto/compas_%s.csv", metric)
+file_laftr  <- sprintf("../pareto/laftr/compas_%s.csv", metric)
+file_zafar  <- sprintf("../pareto/zafar/compas_%s.csv", metric)
 
 output_file <- sprintf("./graphs/compas_%s.pdf", metric)
-
-
-df_with  <- read.csv(with_file, header=T)
-df_without  <- read.csv(without_file, header=T)
-df_with_laftr  <- read.csv(with_laftr_file, header=T)
-df_without_laftr  <- read.csv(without_laftr_file, header=T)
+df_us  <- read.csv(file_us, header=T)
+df_laftr  <- read.csv(file_laftr, header=T)
+df_zafar  <- read.csv(file_zafar, header=T)
 
 
 ggplot() + 
-geom_line(data=df_with, aes(x=error, y=unfairness,color='FairCORELS_sen'), , size=0.2,linetype = "dashed") + 
-geom_point(data=df_with, aes(x=error, y=unfairness,color='FairCORELS_sen'), size=0.9) + 
 
-geom_line(data=df_with_laftr, aes(x=error, y=unfairness,color='LAFTR_sen'), size=0.2, linetype = "dashed") + 
-geom_point(data=df_with_laftr, aes(x=error, y=unfairness,color='LAFTR_sen'), size=0.9) + 
+geom_line(data=df_us, aes(x=error, y=unfairness,color='FairCORELS'),  size=line_size, linetype = "dashed") + 
+geom_point(data=df_us, aes(x=error, y=unfairness,color='FairCORELS'), size=point_size) + 
 
+geom_line(data=df_laftr, aes(x=error, y=unfairness,color='LAFTR'), size=line_size, linetype = "dashed") + 
+geom_point(data=df_laftr, aes(x=error, y=unfairness,color='LAFTR'), size=point_size) + 
 
-geom_line(data=df_without, aes(x=error, y=unfairness,color='FairCORELS'),  size=0.2, linetype = "solid") + 
-geom_point(data=df_without, aes(x=error, y=unfairness,color='FairCORELS'), size=0.9) + 
-
-geom_line(data=df_without_laftr, aes(x=error, y=unfairness,color='LAFTR'), size=0.2, linetype = "solid") + 
-geom_point(data=df_without_laftr, aes(x=error, y=unfairness,color='LAFTR'), size=0.9) + 
+geom_line(data=df_zafar, aes(x=error, y=unfairness,color='C-LR'), size=line_size, linetype = "dashed") + 
+geom_point(data=df_zafar, aes(x=error, y=unfairness,color='C-LR'), size=point_size) + 
 
 scale_color_manual(
-    values = c("FairCORELS_sen" = "darkorange2", "LAFTR_sen" = "darkgreen", "FairCORELS" = "deeppink1", "LAFTR" = "blue" ), 
-    labels = c("FairCORELS_sen" = expression("FairCORELS"["sen"]), "LAFTR_sen" = expression("LAFTR"["sen"]), "FairCORELS" = "FairCORELS", "LAFTR" = "LAFTR")
+    values = c("FairCORELS" = "darkorange2", "LAFTR" = "darkgreen", "C-LR" = "darkred"), 
+    labels = c("FairCORELS" = "FairCORELS", "LAFTR" = "LAFTR", "C-LR" = "C-LR")
 ) + 
 
 theme_bw(base_size=13) + theme(legend.position = c(1, 1), legend.justification = c(1, 1), legend.background = element_blank()) + labs(color='') +
 
 labs(x = "Error", y = expression(Delta[SP]))
 
-
 ggsave(output_file, dpi=300, width=5, height=7)
 
 
-metric <- "equal_opportunity"
-with_file <- sprintf("../pareto/compas_%s_with_dem.csv", metric)
-without_file <- sprintf("../pareto/compas_%s_without_dem.csv", metric)
-with_laftr_file <- sprintf("../pareto/compas_%s_with_dem_laftr.csv", metric)
-without_laftr_file <- sprintf("../pareto/compas_%s_without_dem_laftr.csv", metric)
-
+#Equal opportunity
+metric      <- "equal_opportunity"
+file_us     <- sprintf("../pareto/compas_%s.csv", metric)
+file_laftr  <- sprintf("../pareto/laftr/compas_%s.csv", metric)
+file_zafar  <- sprintf("../pareto/zafar/compas_%s.csv", metric)
 
 output_file <- sprintf("./graphs/compas_%s.pdf", metric)
-
-
-df_with  <- read.csv(with_file, header=T)
-df_without  <- read.csv(without_file, header=T)
-df_with_laftr  <- read.csv(with_laftr_file, header=T)
-df_without_laftr  <- read.csv(without_laftr_file, header=T)
+df_us  <- read.csv(file_us, header=T)
+df_laftr  <- read.csv(file_laftr, header=T)
+df_zafar  <- read.csv(file_zafar, header=T)
 
 
 ggplot() + 
-geom_line(data=df_with, aes(x=error, y=unfairness,color='FairCORELS_sen'), , size=0.2,linetype = "dashed") + 
-geom_point(data=df_with, aes(x=error, y=unfairness,color='FairCORELS_sen'), size=0.9) + 
 
-geom_line(data=df_with_laftr, aes(x=error, y=unfairness,color='LAFTR_sen'), size=0.2, linetype = "dashed") + 
-geom_point(data=df_with_laftr, aes(x=error, y=unfairness,color='LAFTR_sen'), size=0.9) + 
+geom_line(data=df_us, aes(x=error, y=unfairness,color='FairCORELS'),  size=line_size, linetype = "dashed") + 
+geom_point(data=df_us, aes(x=error, y=unfairness,color='FairCORELS'), size=point_size) + 
 
+geom_line(data=df_laftr, aes(x=error, y=unfairness,color='LAFTR'), size=line_size, linetype = "dashed") + 
+geom_point(data=df_laftr, aes(x=error, y=unfairness,color='LAFTR'), size=point_size) + 
 
-geom_line(data=df_without, aes(x=error, y=unfairness,color='FairCORELS'),  size=0.2, linetype = "solid") + 
-geom_point(data=df_without, aes(x=error, y=unfairness,color='FairCORELS'), size=0.9) + 
-
-geom_line(data=df_without_laftr, aes(x=error, y=unfairness,color='LAFTR'), size=0.2, linetype = "solid") + 
-geom_point(data=df_without_laftr, aes(x=error, y=unfairness,color='LAFTR'), size=0.9) + 
+geom_line(data=df_zafar, aes(x=error, y=unfairness,color='C-LR'), size=line_size, linetype = "dashed") + 
+geom_point(data=df_zafar, aes(x=error, y=unfairness,color='C-LR'), size=point_size) + 
 
 scale_color_manual(
-    values = c("FairCORELS_sen" = "darkorange2", "LAFTR_sen" = "darkgreen", "FairCORELS" = "deeppink1", "LAFTR" = "blue" ), 
-    labels = c("FairCORELS_sen" = expression("FairCORELS"["sen"]), "LAFTR_sen" = expression("LAFTR"["sen"]), "FairCORELS" = "FairCORELS", "LAFTR" = "LAFTR")
+    values = c("FairCORELS" = "darkorange2", "LAFTR" = "darkgreen", "C-LR" = "darkred"), 
+    labels = c("FairCORELS" = "FairCORELS", "LAFTR" = "LAFTR", "C-LR" = "C-LR")
 ) + 
 
 theme_bw(base_size=13) + theme(legend.position = c(1, 1), legend.justification = c(1, 1), legend.background = element_blank()) + labs(color='') +
 
-labs(x = "Error", y = expression(Delta[EOpp]))
-
+labs(x = "Error", y = expression(Delta[SP]))
 
 ggsave(output_file, dpi=300, width=5, height=7)
 
 
-
-metric <- "equalized_odds"
-with_file <- sprintf("../pareto/compas_%s_with_dem.csv", metric)
-without_file <- sprintf("../pareto/compas_%s_without_dem.csv", metric)
-with_laftr_file <- sprintf("../pareto/compas_%s_with_dem_laftr.csv", metric)
-without_laftr_file <- sprintf("../pareto/compas_%s_without_dem_laftr.csv", metric)
-
+#Equalized odds
+metric      <- "equalized_odds"
+file_us     <- sprintf("../pareto/compas_%s.csv", metric)
+file_laftr  <- sprintf("../pareto/laftr/compas_%s.csv", metric)
+file_zafar  <- sprintf("../pareto/zafar/compas_%s.csv", metric)
 
 output_file <- sprintf("./graphs/compas_%s.pdf", metric)
-
-
-df_with  <- read.csv(with_file, header=T)
-df_without  <- read.csv(without_file, header=T)
-df_with_laftr  <- read.csv(with_laftr_file, header=T)
-df_without_laftr  <- read.csv(without_laftr_file, header=T)
+df_us  <- read.csv(file_us, header=T)
+df_laftr  <- read.csv(file_laftr, header=T)
+df_zafar  <- read.csv(file_zafar, header=T)
 
 
 ggplot() + 
-geom_line(data=df_with, aes(x=error, y=unfairness,color='FairCORELS_sen'), , size=0.2,linetype = "dashed") + 
-geom_point(data=df_with, aes(x=error, y=unfairness,color='FairCORELS_sen'), size=0.9) + 
 
-geom_line(data=df_with_laftr, aes(x=error, y=unfairness,color='LAFTR_sen'), size=0.2, linetype = "dashed") + 
-geom_point(data=df_with_laftr, aes(x=error, y=unfairness,color='LAFTR_sen'), size=0.9) + 
+geom_line(data=df_us, aes(x=error, y=unfairness,color='FairCORELS'),  size=line_size, linetype = "dashed") + 
+geom_point(data=df_us, aes(x=error, y=unfairness,color='FairCORELS'), size=point_size) + 
 
+geom_line(data=df_laftr, aes(x=error, y=unfairness,color='LAFTR'), size=line_size, linetype = "dashed") + 
+geom_point(data=df_laftr, aes(x=error, y=unfairness,color='LAFTR'), size=point_size) + 
 
-geom_line(data=df_without, aes(x=error, y=unfairness,color='FairCORELS'),  size=0.2, linetype = "solid") + 
-geom_point(data=df_without, aes(x=error, y=unfairness,color='FairCORELS'), size=0.9) + 
-
-geom_line(data=df_without_laftr, aes(x=error, y=unfairness,color='LAFTR'), size=0.2, linetype = "solid") + 
-geom_point(data=df_without_laftr, aes(x=error, y=unfairness,color='LAFTR'), size=0.9) + 
+geom_line(data=df_zafar, aes(x=error, y=unfairness,color='C-LR'), size=line_size, linetype = "dashed") + 
+geom_point(data=df_zafar, aes(x=error, y=unfairness,color='C-LR'), size=point_size) + 
 
 scale_color_manual(
-    values = c("FairCORELS_sen" = "darkorange2", "LAFTR_sen" = "darkgreen", "FairCORELS" = "deeppink1", "LAFTR" = "blue" ), 
-    labels = c("FairCORELS_sen" = expression("FairCORELS"["sen"]), "LAFTR_sen" = expression("LAFTR"["sen"]), "FairCORELS" = "FairCORELS", "LAFTR" = "LAFTR")
+    values = c("FairCORELS" = "darkorange2", "LAFTR" = "darkgreen", "C-LR" = "darkred"), 
+    labels = c("FairCORELS" = "FairCORELS", "LAFTR" = "LAFTR", "C-LR" = "C-LR")
 ) + 
 
 theme_bw(base_size=13) + theme(legend.position = c(1, 1), legend.justification = c(1, 1), legend.background = element_blank()) + labs(color='') +
 
-labs(x = "Error", y = expression(Delta[EOdds]))
-
+labs(x = "Error", y = expression(Delta[SP]))
 
 ggsave(output_file, dpi=300, width=5, height=7)
 
