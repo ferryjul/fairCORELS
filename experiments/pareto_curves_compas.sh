e88502a82afc0690192b6032c38b94d013ddc018
black-women-mirror <a.u.matchi@gmail.com>
2019-12-29 22:42:24 -0500

    script for experiments

--- experiments/pareto_curves_compas.sh
+++
@@ -1,8 +0,0 @@
-python pareto_curves.py --exp=results --id=2 --attr=1 --m=1
-python pareto_curves.py --exp=results --id=2 --attr=2 --m=1 
-
-python pareto_curves.py --exp=results --id=2 --attr=1 --m=4
-python pareto_curves.py --exp=results --id=2 --attr=2 --m=4
-
-python pareto_curves.py --exp=results --id=2 --attr=1 --m=5
-python pareto_curves.py --exp=results --id=2 --attr=2 --m=5
--- experiments/plotting_scripts/analysis_single.sh
+++
@@ -1,7 +0,0 @@
-Rscript analysis_single.R --exp=results_4M --id=1 --attr=1 --m=1
-
-Rscript analysis_single.R --exp=results_4M --id=1 --attr=1 --m=3
-
-Rscript analysis_single.R --exp=results_4M --id=1 --attr=1 --m=4
-
-Rscript analysis_single.R --exp=results_4M --id=1 --attr=1 --m=5
