f1a0249cd28b2cbda71b1bdd511fa190f8712cf3
black-women-mirror <a.u.matchi@gmail.com>
2020-01-17 12:07:06 -0500

    script for experiments

---
+++ examples/adult-no-sens-arg.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --time=24:00:00
+#SBATCH --array=1-6
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=150G  
+#SBATCH --nodes=1
+
+
+python evalStrats-adult.py --strat=1 --forbidSensAttr=1 --metric=$SLURM_ARRAY_TASK_ID
+
---
+++ examples/launch.sh
@@ -0,0 +1,25 @@
+sbatch adult-no-sens-arg-2.sh
+sleep 1
+
+sbatch  adult-no-sens-arg-3.sh
+sleep 1
+
+sbatch adult-no-sens-arg-4.sh
+sleep 1
+
+sbatch adult-no-sens-arg.sh
+sleep 1
+
+
+sbatch adult-sens-arg-2.sh
+sleep 1
+
+sbatch adult-sens-arg-3.sh
+sleep 1
+
+sbatch adult-sens-arg-4.sh
+sleep 1
+
+sbatch  adult-sens-arg.sh
+
+
