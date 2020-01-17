82bfce31f36deb7e51d9871235095862ff20d59a
black-women-mirror <a.u.matchi@gmail.com>
2020-01-17 12:08:43 -0500

    script for experiments

--- examples/adult-sens-arg.sh
+++ examples/adult-sens-arg.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --time=24:00:00
 #SBATCH --array=1-6
-#SBATCH --mem-per-cpu=6000
-#SBATCH --cpus-per-task=15
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=150G  
 #SBATCH --nodes=1
 
 
