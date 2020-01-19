497376cd0dd372cee82140b4e361a737a7799098
black-women-mirror <a.u.matchi@gmail.com>
2020-01-19 00:58:33 -0500

    script for experiments

--- get_strategy_results.sh
+++ get_strategy_results.sh
@@ -1,1 +1,1 @@
-scp -r umaivodj@mp2.calculcanada.ca:~/scratch/fairCORELS/examples/results/ examples/results
+scp -r umaivodj@mp2.calculcanada.ca:~/scratch/fairCORELS/search_strategies/results/ search_strategies/results
