98dbecebda2a8f3302828b3f294995d8bcdaa613
black-women-mirror <a.u.matchi@gmail.com>
2020-01-18 21:49:32 -0500

    script for experiments

--- search_strategies/strat_1_with.sh
+++ search_strategies/strat_1_with.sh
@@ -2,12 +2,12 @@
 #SBATCH --time=00:10:00
 #SBATCH --array=1
 #SBATCH --ntasks=60
-#SBATCH --mem-per-cpu=2G
+#SBATCH --mem-per-cpu=100M
 
 #SBATCH --account=def-gambsseb
 #SBATCH --mail-user=a.u.matchi@gmail.com
 #SBATCH --mail-type=ALL
 
 
-srun python bench.py --id=6 --metric=$SLURM_ARRAY_TASK_ID --attr=1 --strat=1
+srun python bench.py --dataset=6 --metric=$SLURM_ARRAY_TASK_ID --attr=1 --strat=1
 
