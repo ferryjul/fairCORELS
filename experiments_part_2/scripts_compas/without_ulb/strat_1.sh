4454202e9f7e93cea796b45eb6dfd6569bc2b4e9
black-women-mirror <a.u.matchi@gmail.com>
2020-01-24 01:33:52 -0500

    script for experiments

--- experiments_part_2/scripts_compas/without_ulb/strat_1.sh
+++ experiments_part_2/scripts_compas/without_ulb/strat_1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --time=01:30:00
+#SBATCH --time=02:00:00
 #SBATCH --array=1-6
 #SBATCH --ntasks=60
-#SBATCH --mem-per-cpu=5G
+#SBATCH --mem-per-cpu=6G
 
 #SBATCH --account=def-gambsseb
 #SBATCH --mail-user=a.u.matchi@gmail.com
--- experiments_part_2/scripts_compas/without_ulb/strat_4.sh
+++ experiments_part_2/scripts_compas/without_ulb/strat_4.sh
@@ -2,7 +2,7 @@
 #SBATCH --time=05:00:00
 #SBATCH --array=1-6
 #SBATCH --ntasks=60
-#SBATCH --mem-per-cpu=10G
+#SBATCH --mem-per-cpu=7G
 
 #SBATCH --account=def-gambsseb
 #SBATCH --mail-user=a.u.matchi@gmail.com
