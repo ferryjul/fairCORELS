d78607cd44252fd1aac998fa4e6a86d3c88e5f7f
black-women-mirror <a.u.matchi@gmail.com>
2020-01-24 14:05:27 -0500

    script for experiments

--- experiments_part_2/scripts_adult/without_ulb/strat_2.sh
+++ experiments_part_2/scripts_adult/without_ulb/strat_2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --time=02:00:00
+#SBATCH --time=05:00:00
 #SBATCH --array=1-6
-#SBATCH --ntasks=60
+#SBATCH --ntasks=75
 #SBATCH --mem-per-cpu=6G
 
 #SBATCH --account=def-gambsseb
