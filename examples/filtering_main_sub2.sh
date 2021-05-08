ffb055ca50d3466d6435a8ea982d0683dc73d004
Julien Ferry <jferry@nirvana>
2021-05-09 00:30:50 +0200

    e

--- examples/filtering_main_sub2.sh
+++ examples/filtering_main_sub2.sh
@@ -2,12 +2,12 @@
 
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=5000
+#SBATCH --mem-per-cpu=4500
 #SBATCH --time=00:35:00
 #SBATCH --job-name=filtering
 #SBATCH -o slurmout_%A.out
 #SBATCH -e slurmout_%A.errarray
-#SBATCH --array=0-5039
+#SBATCH --array=0-1680
 
 python3 filtering_eval_auto_sub2.py --expe=${SLURM_ARRAY_TASK_ID}
 
