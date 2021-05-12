52aac51fe8fa849023e5b82acbce5730b0c3e540
Julien Ferry <jferry@nirvana>
2021-05-12 22:29:24 +0200

    analyze script

--- examples/analyze_rerun_missing.py
+++ examples/analyze_rerun_missing.py
@@ -1,5 +1,5 @@
-import pandas as pd
-
+#import pandas as pd
+import os.path
 
 max_times=[120, 300, 400, 500, 600, 900, 1200] 
 
@@ -15,7 +15,7 @@ seeds = []
 for i in range(0,20):
     seeds.append(i)
 
-metrics=[3, 4, 5]#, 4, 5]
+metrics=[1, 3, 4, 5]#, 4, 5]
 
 filteringModes = [0, 1, 2]
 # -----------------------------------------------------
@@ -32,7 +32,7 @@ expe_ids = []
 for i in range(nb_expes):
     expe_ids.append(i)
 print("Needed to run %d expes." %nb_expes)
-
+cnt = 0
 torerun = []
 for expe_id in expe_ids:
     dataset = cart_product[expe_id][0]
@@ -42,14 +42,18 @@ for expe_id in expe_ids:
     filteringMode = cart_product[expe_id][4]
     for seed in seeds:
         fileName = './results/%s%s_eps%f_metric%d_LB%d_%s_tLimit%d_single_seed%d%s.csv' %(folderPrefix, dataset, epsilon, fairnessMetric, filteringMode, policy, max_time, seed, archSuffix)
-        if expe_id == 116:
-            print(fileName)
-        try:
+        if os.path.isfile(fileName):
+            if not expe_id in torerun:
+                torerun.append(expe_id)
+            else:
+                cnt+=1
+        '''try:
             fileContent = pd.read_csv(fileName)
         except FileNotFoundError as not_found:
             #print("Missing seed %d, max_time=%d, metric= %d, epsilon=%lf, expe_id=%d" %(seed, max_time, fairnessMetric, epsilon, expe_id))
             if not expe_id in torerun:
-                torerun.append(expe_id)
+                torerun.append(expe_id)'''
+        
 
 if len(torerun) == 0:
     print("All files found.")
@@ -57,7 +61,8 @@ else:
     print("Expes to re-run are: ")
     # print without white space so that copy-paste is easy
     print(str(torerun).replace(" ", ""))
-    print("total = %d expes" %len(torerun))
+    print("total = %d expes to re-run" %len(torerun))
     '''for i in range(nb_expes):
         if not i in torerun:
             print("expe ", i, " ok")'''
+print("Found %d files" %cnt)
