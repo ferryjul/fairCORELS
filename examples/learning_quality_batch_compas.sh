63c09d148151276dcecc4870e9f82e14d91853c2
Julien Ferry <jferry@nirvana>
2021-05-21 11:16:10 +0200

    adds learning quality expes for compas

---
+++ examples/learning_quality_batch_compas.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH -n 5
+#SBATCH --mem-per-cpu=4500
+#SBATCH --time=00:30:00
+#SBATCH --job-name=learning
+#SBATCH -o slurmout_%A.out
+#SBATCH -e slurmout_%A.errarray
+#SBATCH --constraint=broadwell
+#SBATCH --array=0-0
+
+srun -W 1200 -n 5 python3 learning_quality_compas.py --expe=${SLURM_ARRAY_TASK_ID}
+                                                                                                                 
